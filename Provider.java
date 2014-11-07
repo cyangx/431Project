0672d0ee74915446d8e1e0b97e23b374a36a1d05
garrett-brennan <garrett@patrickbrennan.com>
2014-11-07 07:47:10 -0600

    Added First files
    Added the Factory, Member, Provider, and Service classes

---
+++ Provider.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Garrett
+ * @since  11/7/14
+ */
+public class Provider {
+    private String _name;
+    private int _ID;
+    private String _address;
+    private String _city;
+    private String _state;
+    private String _zipcode;
+    
+    public Provider(String Name, int ID, String Address, String City, String State, String ZipCode)
+    {
+        // TODO: create the get and set methods and use those in the constructor.
+        //          That will ensure that you abide by the field lengths
+    }
+}
---
+++ Service.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Garrett
+ * @since 11/7/14
+ */
+public class Service {
+    private String _name;
+    private int _ID;
+    private double _fee;;
+    
+    public Service(String Name, int ID, double Fee)
+    {
+        // TODO: create the get and set methods and use those in the constructor.
+        //          That will ensure that you abide by the field lengths
+    }
+}
