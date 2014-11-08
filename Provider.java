4040f7b3fbc60ff8d637bb92bb7c2b5800943796
ZombieGadget <wolfpack99997@hotmail.com>
2014-11-08 13:02:20 -0600

    Prov V1
    Provider and ProviderList Version 1
    Does compile. If it doesn't on your end, remove "package sogeti" at the
    top.

--- Provider.java
+++ Provider.java
@@ -1,19 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sogeti;
+
 /**
  *
- * @author Garrett
- * @since  11/7/14
+ * @author Brandon
  */
-public class Provider {
+import java.util.*;
+import java.io.*;
+
+public class Provider implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private List services = new LinkedList();
+    
+    
+  
     private String _name;
     private int _ID;
     private String _address;
     private String _city;
     private String _state;
     private String _zipcode;
+    private String bankName;
+    private int accountNum;
+    private double providerAmount;
     
-    public Provider(String Name, int ID, String Address, String City, String State, String ZipCode)
+    public Provider(String name, int ID, String address, String city, String state, String zipCode)
     {
-        // TODO: create the get and set methods and use those in the constructor.
-        //          That will ensure that you abide by the field lengths
+        this._name = name;
+        this._ID = ID;
+        this._address = address;
+        this._city = city;
+        this._state = state;
+        this._zipcode = zipCode;
+        //Factory should also account for account name
+        //And account number of provider
+    }
+    
+    //Returns the list of services
+    public Iterator getListOfServices(){
+        return services.iterator();
+    }
+    
+    //Returns the provider name
+    public String getProviderName(){
+        return _name;
+    }
+    
+    //Changes the provider name
+    public void setProviderName(String providerName){
+        this._name = providerName;
+    }
+    
+    //returns the amount owed to the provider
+    public double getProviderAmt(){
+        return providerAmount;
+    }
+    
+    //Adds the amount owed to the provider
+    public void setProviderAmt(double providerAmount){
+        this.providerAmount += providerAmount;
+    }
+    
+    //Returns the ID of the provider
+    public int getId(){
+        return this._ID;
+    } 
+    
+    //Don't know what this does yet or how to implement
+    public void addServiceRecord(){
+        
+    }
+    
+    //Updates the provider bank account. 
+    public void update(String bankName, int accountNum){
+        this.bankName = bankName;
+        this.accountNum = accountNum;
     }
 }
