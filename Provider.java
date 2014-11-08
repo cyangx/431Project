7b85935e475e34d3aacc26e47b519cc074f286ee
garrett-brennan <garrett@patrickbrennan.com>
2014-11-08 08:35:41 -0600

    Revert "Up1"
    This reverts commit 282aacf0c96904007ad5d537a0a2d1e0bc333066.

--- Provider.java
+++ Provider.java
@@ -1,61 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sogeti;
-
 /**
  *
- * @author Brandon
+ * @author Garrett
+ * @since  11/7/14
  */
-import java.util.*;
-import java.io.*;
-
-public class Provider implements Serializable{
-    private static final long serialVersionUID = 1L;
-    private List services = new LinkedList();
+public class Provider {
+    private String _name;
+    private int _ID;
+    private String _address;
+    private String _city;
+    private String _state;
+    private String _zipcode;
     
-    
-    String providerName;
-    String id;
-    double providerAmount;
-    
-    public Provider(String providerName, double providerAmount, String id){
-        this.providerName = providerName;
-        this.providerAmount = providerAmount;
-        this.id = id;
-    }
-    
-    public Iterator getListOfServices(){
-        return services.iterator();
-    }
-    
-    public String getProviderName(){
-        return providerName;
-    }
-    
-    public void setProviderName(String providerName){
-        this.providerName = providerName;
-    }
-    
-    public double getProviderAmt(){
-        return providerAmount;
-    }
-    
-    public void setProviderAmt(double providerAmount){
-        this.providerAmount = providerAmount;
-    }
-    
-    public String getId(){
-        return id;
-    } 
-    
-    public void addServiceRecord(){
-        
-    }
-    
-    public void update(){
-        
+    public Provider(String Name, int ID, String Address, String City, String State, String ZipCode)
+    {
+        // TODO: create the get and set methods and use those in the constructor.
+        //          That will ensure that you abide by the field lengths
     }
 }
