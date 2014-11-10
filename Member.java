50d79a6a86aa33f190d48eb0d809d8a8eb83754c
cyangx <yangpride@msn.com>
2014-11-10 11:32:38 -0600

    Added Member and MemberList

--- Member.java
+++ Member.java
@@ -1,19 +1,69 @@
 /**
  *
  * @author Garrett
+ * @author Cha
  * @since 11/7/14
  */
 public class Member {
+
     private String _name;
     private int _ID;
     private String _address;
     private String _city;
     private String _state;
     private String _zipcode;
-    
-    public Member(String Name, int ID, String Address, String City, String State, String ZipCode)
-    {
-        // TODO: create the get and set methods and use those in the constructor.
-        //          That will ensure that you abide by the field lengths
+    private boolean _suspendStatus;
+
+    public Member(String Name, String Address, String City, String State, String ZipCode) {
+        this._name = Name;
+        this._address = Address;
+        this._city = City;
+        this._state = State;
+        this._zipcode = ZipCode;
+        // set by the ID factory
+        //this._ID = ;
+
+    }
+
+    public void update(String Name, String Address, String City, String State, String ZipCode) {
+        this._name = Name;
+        this._address = Address;
+        this._city = City;
+        this._state = State;
+        this._zipcode = ZipCode;
+
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public int getID() {
+        return _ID;
+    }
+
+    public String getAddress() {
+        return _address;
+    }
+
+    public String getCity() {
+        return _city;
+    }
+
+    public String getState() {
+        return _state;
+    }
+
+    public String getZipcode() {
+        return _zipcode;
+    }
+
+    public boolean isSuspend() {
+        return _suspendStatus;
+    }
+
+    // set suspend() and unsuspend()
+    public void setSuspendStatus(boolean _suspendStatus) {
+        this._suspendStatus = _suspendStatus;
     }
 }
