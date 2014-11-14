ae51aa57bdd59773a79c2c352592e439ef396d01
garrett-brennan <Garrett@Garrett-Brennans-MacBook-Pro-2.local>
2014-11-13 20:21:47 -0600

    Stubbing all day

--- src/pkg431/ProviderSystem.java
+++ src/pkg431/ProviderSystem.java
@@ -6,4 +6,12 @@ package pkg431;
  */
 public class ProviderSystem {
     
+    // TODO: Submit a claim
+    
+    // TODO bill a claim (maybe just make this part of submit)
+    
+    // TODO print provider directory
+    
+    // TODO Print provider report?
+    
 }
